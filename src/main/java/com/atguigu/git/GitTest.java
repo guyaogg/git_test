d3a2e904136b69d0435df9d4b8c189d2f7a9f17d
Guyaogg <3039744913@qq.com>
2021-07-21 15:39:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author guyao
+ * @create 2021-07-21 15:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
